1470d86fc29588e6157dd2d61739c9a6f12e61a0
krisna0107 <krisnayanajavista@gmail.com>
2021-11-13 21:20:30 +0700

    first commit

---
+++ app/Http/Controllers/BanlistController.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Banlist;
+use Illuminate\Http\Request;
+
+class BanlistController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    public function getBanList(Request $request)
+    {
+        if (!$request->steam) {
+            return response()->json([
+                "status" => "400",
+                "message"   => "Bad Request"
+            ], 500);
+        }
+        $banlist = Banlist::orWhere('steam', $request->steam);
+        if ($request->license) {
+            $banlist = $banlist->orWhere('license', $request->license);
+        }
+
+        if ($request->discord) {
+            $banlist = $banlist->orWhere('discord', $request->discord);
+        }
+
+        if ($request->xbl) {
+            $banlist = $banlist->orWhere('xbl', $request->xbl);
+        }
+
+        if ($request->liveid) {
+            $banlist = $banlist->orWhere('liveid', $request->liveid);
+        }
+
+        $banlist = $banlist->first();
+
+        if ($banlist!=null) {
+            return response()->json([
+                "status" => "200",
+                "message"   => $banlist->reason
+            ], 200);
+        }
+
+        return response()->json([
+            "status" => "404",
+            "message"   => "Not Banned"
+        ], 404);
+    }
+}
---
+++ app/Models/Banlist.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use DateTimeInterface;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Banlist extends Model
+{
+    protected $table = 'banlist';
+    protected function serializeDate(DateTimeInterface $date)
+    {
+        return $date->format('Y-m-d H:i:s');
+    }
+}
